4376fc563c998a98b15077949cc9971c0a952d27
Tomas David Mendoza Aguilar <davidmendoza1234560@gmail.com>
2020-11-06 17:20:49 -0600

    Ecuacion version 1.0

---
+++ app/src/main/java/com/example/calculadoraecuacion/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.calculadoraecuacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.app.AlertDialog;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText a = (EditText)findViewById(R.id.v1);
+        EditText b = (EditText)findViewById(R.id.v2);
+        EditText c = (EditText)findViewById(R.id.v3);
+        Button   operacion = (Button)findViewById(R.id.btn);
+        TextView valor_x1 = (TextView)findViewById(R.id.x1);
+        TextView valor_x2 = (TextView)findViewById(R.id.x2);
+        TextView dis = (TextView)findViewById(R.id.tvD);
+        operacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                try {// para encapsular un error de ingreso de datos como String en los EditText
+                    String xA = a.getText().toString();
+                    String xB = b.getText().toString();
+                    String xC = c.getText().toString();
+                    //verificación de que los campos esten llenos
+
+                    if (xA.isEmpty() || xB.isEmpty() || xC.isEmpty()) {
+                        if (xA.isEmpty()) {
+                            a.setError("Ingresar Valor");
+                        }
+                        if (xB.isEmpty()) {
+                            b.setError("Ingresar Valor");
+                        }
+                        if (xC.isEmpty()) {
+                            c.setError("Ingresar Valor");
+                        }
+                    } else {
+                        double a1 = Double.parseDouble(xA);
+                        double b1 = Double.parseDouble(xB);
+                        double c1 = Double.parseDouble(xC);
+
+                        //verificación El coeficiente a no puede ser igual a cero
+                        if (a1 == 0) {
+                            AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+                            alerta.setMessage("El coeficiente a no puede ser igual a cero").setNegativeButton("Aceptar", null).create().show();
+                        } else{
+                            double discriminante = Math.pow(b1, 2) - 4 * a1 * c1;
+                            if (discriminante >= 0) {
+                                if (discriminante == 0) {
+                                    double x = -b1 / (2 * a1);
+                                    AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+                                    alerta.setMessage("La raíz única es: "+x+"\n"+"Discriminante: "+discriminante).setNegativeButton("Aceptar", null).create().show();
+                                    valor_x1.setText("");
+                                    valor_x2.setText("");
+
+                                    a.setText("");
+                                    b.setText("");
+                                    c.setText("");
+                                } else {
+                                    double x1, x2;
+                                    x1 = (-b1 + Math.sqrt(discriminante)) / (2 * a1);
+                                    x2 = (-b1 - Math.sqrt(discriminante)) / (2 * a1);
+
+                                    valor_x1.setText("X1: "+x1);
+                                    valor_x2.setText("X2: "+x2);
+                                    dis.setText("Discriminante: "+discriminante);
+                                    Toast.makeText(MainActivity.this,"X1: "+x1 +"\n"+"X2: "+x2 ,Toast.LENGTH_LONG).show();
+                                    a.setText("");
+                                    b.setText("");
+                                    c.setText("");
+                                }
+                            }else if(discriminante < 0){
+                                AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+                                alerta.setMessage("La discriminante es menor que cero, como no existen las raíces de números negativos, la ecuación no tendrá soluciones.").setNegativeButton("Aceptar", null).create().show();
+                                dis.setText("Discriminante: "+discriminante);
+                                valor_x1.setText("");
+                                valor_x2.setText("");
+                                a.setText("");
+                                b.setText("");
+                                c.setText("");
+                            }
+
+                        }
+
+                    }
+                }catch (Exception e){
+                    //envio del mensaje que ocurrio un error
+                    AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+                    alerta.setMessage("Haz ingresado un valor incorrecto (caracter, símbolo, etc.)").setNegativeButton("Aceptar", null).create().show();
+                    valor_x1.setText("");
+                    valor_x2.setText("");
+                    dis.setText("");
+                }
+
+
+
+            }
+        });
+
+
+
+    }
+}
